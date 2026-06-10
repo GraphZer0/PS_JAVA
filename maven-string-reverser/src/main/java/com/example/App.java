f6f86f7353e2a80de44c3794749a98333121e41c
GraphZer0 <alx.krstv@gmail.com>
2026-06-10 16:29:07 +0300

    Системы автоматической сборки Java-проектов

---
+++ maven-string-reverser/src/main/java/com/example/App.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.apache.commons.lang3.StringUtils;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        String text = "Hello World!";
+        String reversed = StringUtils.reverse(text);
+
+        System.out.println(reversed);
+    }
+}
