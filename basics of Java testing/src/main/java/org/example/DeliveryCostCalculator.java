c6a1363bbf83b8f5f40892756bec039e1510a9e0
GraphZer0 <alx.krstv@gmail.com>
2026-06-18 16:12:54 +0300

    Основы тестирования на Java
    Практическая работа № 2: основы тестирования на Java

---
+++ basics of Java testing/src/main/java/org/example/DeliveryCostCalculator.java
@@ -0,0 +1,60 @@
+package org.example;
+
+public class DeliveryCostCalculator {
+
+    private static final double MIN_DELIVERY_COST = 400.0;
+
+    public double calcatale(double distance, String size, boolean fragile, String workload) {
+
+        double cost = 0;
+
+        if (fragile && distance > 30) {
+            throw  new IllegalArgumentException(
+                    "Хрупкие грузы нельзя перевозить на растояние более 30 км");
+        }
+
+        //растояния
+        if (distance > 30) {
+            cost += 300;
+        } else if (distance > 2) {
+            cost += 100;
+        }  else {cost += 50;}
+
+        //Габариты
+        if ("large".equalsIgnoreCase(size)) {
+            cost += 200;
+        } else if ("small".equalsIgnoreCase(size)) {
+            cost += 100;}
+        else { throw  new IllegalArgumentException("Некорректный размер груза"); }
+
+        //Хрупкость
+
+        if (fragile) {
+            cost += 300;
+        }
+
+        //Коэфициент загружености
+
+        double coefficient;
+
+        switch (workload.toLowerCase()) {
+            case "very_high":
+                coefficient = 1.6;
+                break;
+            case "high":
+                coefficient = 1.4;
+                break;
+            case "elevated":
+                coefficient = 1.2;
+                break;
+            case "normal":
+                coefficient = 1.0;
+                break;
+            default:
+                throw new IllegalArgumentException("Некорректный уровень загруженности");
+        }
+        cost += coefficient;
+
+        return Math.max(cost, MIN_DELIVERY_COST);
+    }
+}
